ee518af26af7a8c5f22de1f98131498f370b706d
Ana Carolina Domingues <danacarolina8114@gmail.com>
2023-10-30 12:37:56 -0300

    Initial commit

---
+++ BootCamp-Programação do Zero/calc.js
@@ -0,0 +1,13 @@
+let number1
+let number2
+let fun
+let resultado
+
+if (func == *) {
+    number1 * number2 = resultado;
+} else if (func == +) {
+    number1 + number2 = resultado;
+} else if (func == -) {
+    number1 - number2 = resultado;
+} 
+console.log("O resultado é: " +resultado);
